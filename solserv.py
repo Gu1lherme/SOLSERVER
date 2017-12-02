a88d3857416f597af82c1e4472c6a917e90da87b
yuriilima <31909166+yuriilima@users.noreply.github.com>
2017-12-01 20:26:45 -0400

    Update solserv.py

--- solserv.py
+++ solserv.py
@@ -37,7 +37,7 @@ def protected():
     return redirect(url_for('index'))
 
 
-@app.route('/')
+@app.route('/index')
 @flask_login.login_required
 def index():
     return render_template('index.html')
